367971bfabfdf557c4a9e200816f1dc43ed2dffc
pkErbynn <john.erbynn@gmail.com>
2019-11-28 13:34:17 +0000

    multi threads

--- src/main/java/io/turntabl/my/Threads2.java
+++ src/main/java/io/turntabl/my/Threads2.java
@@ -1,20 +1,47 @@
 package io.turntabl.my;
 
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Threads2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         List<Thread> _10Threads = IntStream.range(1, 11).mapToObj(
                 e -> new Thread(() -> {
-                    while (!Thread.interrupted()) System.out.println("Thread " + e + "Uninterrupted");
+                    while (!Thread.interrupted()) System.out.println("Thread " + e + " Interrupted");
                 }
                 )
         ).collect(Collectors.toList());
 
-        _10Threads.stream().forEach(e-> e.start());
+        _10Threads.stream().forEach(e-> e.start ());
+
+        System.out.println("Active threads running: " + Thread.activeCount());
+
+//        Random random = new Random();
+//        while (Thread.activeCount() != 1) {
+
+
+        _10Threads.stream().forEach(thread -> {
+            try {
+                thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        _10Threads.stream().forEach(thread -> thread.interrupt());
+
+
+        _10Threads.stream().forEach(thread -> {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
 
 
     }
